b14ce525d6792bc5f5469e06c07828128f76574c
facc77 <ferrerfacundo96@gmail.com>
2022-08-16 20:23:00 -0300

    useFetch api created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import useFetch from './hooks/useFetch';
 
 function App() {
+  const { data, loading, error } = useFetch(
+    'http://api.weatherapi.com/v1',
+    '6be8c28794924ed8a2a184922222905',
+    'tuc',
+  );
+
+  console.log(data, loading, error);
   return (
     <div className='App'>
       <Sidebar />
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function useFetch(url, key, place = '') {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const completeUrl = `${url}/search.json?key=${key}&q=${place}`;
+
+  useEffect(() => {
+    axios
+      .get(completeUrl)
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError('An error occurred', err);
+      });
+  }, [completeUrl]);
+
+  return { data, loading, error };
+}
+export default useFetch;
